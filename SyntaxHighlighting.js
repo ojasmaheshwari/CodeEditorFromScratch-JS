d918d30d3806b9c73e15db639055d310a6ff1dea
Ojas Maheshwari <ojasmaheshwari2006@gmail.com>
2024-12-25 14:02:19 +0530

    Tab working

--- SyntaxHighlighting.js
+++ SyntaxHighlighting.js
@@ -1,8 +1,9 @@
-const keywords = ['class', 'void', 'int', 'return', 'pragma', 'include', 'public', 'private', 'const', 'static', 'true', 'false', 'null', 'NULL', 'nullptr', 'for', 'if', 'while', 'else'];
+const keywords = ['class', 'void', 'int', 'return', 'pragma', 'include', 'public', 'private', 'const', 'static', 'true', 'false', 'null', 'NULL', 'nullptr', 'for', 'if', 'while', 'else', 'enum', 'struct'];
 
 const ColorMapping = {
 	keywords: "#569cd6",
 	doubleQuotationStrings: "#ce9178",
+	singleQuotationStrings: "#ce9178",
 	comments: "gray",
 };
 
@@ -20,16 +21,20 @@ const RegexRules = {
 	singleLineComments: new RegExp('(\/\/.*)', 'g'),
 	multiLineComments: new RegExp(/(\/\*[^*]*\*+(?:[^\/*][^*]*\*+)*\/)/gm),
 	doubleQuotationStrings: new RegExp('"(.*?)"', 'g'),
+	singleQuotationStrings: new RegExp(/'(.*?)'/g)
 };
 
 export function highlight(element) {
 	console.log(RegexRules.multiLineComments);
 	console.log(RegexRules.multiLineComments.test(element.innerText));
 
-	// let replaceBy = element.innerText
-	// 	.replace(RegexRules.multiLineComments, coloredSpan('$1', 'gray'));
-	// element.innerHTML = replaceBy.split('\n').join('<br/>');
+	/*
+	let replaceBy = element.innerText
+		.replace(RegexRules.multiLineComments, coloredSpan('$1', 'gray'));
+	element.innerHTML = replaceBy.split('\n').join('<br/>');
+	*/
 
+	/*
 	const replaceBy = element.innerText
 		.replace('<', '&lt')
 		.replace('>', '&gt')
@@ -38,16 +43,16 @@ export function highlight(element) {
 		.replace(RegexRules.multiLineComments, coloredSpan('$1', ColorMapping.comments))
 		.replace(RegexRules.singleLineComments, coloredSpan('<em>$1</em>', ColorMapping.comments))
 	element.innerHTML = replaceBy.split('\n').join('<br/>');
+	*/
 
-	/*
 	for (const node of element.children) {
-		replaceBy = node.innerText
+		const replaceBy = node.innerText
 			.replace('<', '&lt')
 			.replace('>', '&gt')
 			.replace(RegexRules.doubleQuotationStrings, coloredSpan('"$1"', ColorMapping.doubleQuotationStrings))
-			.replace(RegexRules.singleLineComments, '<em>$1</em>')
+			.replace(RegexRules.singleQuotationStrings, coloredSpan('\'$1\'', ColorMapping.singleQuotationStrings))
+			.replace(RegexRules.singleLineComments, coloredSpan('<em>$1</em>', ColorMapping.comments))
 			.replace(RegexRules.keywords, coloredSpan('$1', ColorMapping.keywords))
 		node.innerHTML = replaceBy.split('\n').join('<br/>');
 	}
-	*/
 }
